baa9c8391743d573d1a671f23efdcaf7335f51b1
mcacrz <mca.crz@gmail.com>
2022-07-13 12:48:02 -0300

    5 files modified, 3 added, 1 deleted

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar css
+import 'quasar/src/assets/css/index.sass';
+import App from './App.vue';
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app');
